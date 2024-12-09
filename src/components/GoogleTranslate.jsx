44a65c855138d222f58e69fba03b5f1df1b01fab
muhammadfaizibr <work.faizmuhammad@gmail.com>
2024-12-09 13:43:57 +0500

    translator bugs fixed

--- src/components/GoogleTranslate.jsx
+++ src/components/GoogleTranslate.jsx
@@ -1,31 +1,32 @@
 import React, { useEffect } from 'react';
 
 const GoogleTranslate = () => {
-   
   const googleTranslateElementInit = () => {
+    if (!window.google || !window.google.translate) return;
     new window.google.translate.TranslateElement(
       {
         pageLanguage: "en",
         autoDisplay: false,
-        includedLanguages: 'ur,pa,sd,ps', // Specify supported languages (Urdu, Punjabi, Sindhi, Pashto)
+        includedLanguages: "ur,pa,sd,ps",
         layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
       },
       "google_translate_element"
     );
   };
+
   useEffect(() => {
-    var addScript = document.createElement("script");
-    addScript.setAttribute(
-      "src",
-      "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-    );
-    document.body.appendChild(addScript);
+    if (!document.querySelector("script[src='//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit']")) {
+      var addScript = document.createElement("script");
+      addScript.setAttribute(
+        "src",
+        "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+      );
+      document.body.appendChild(addScript);
+    }
     window.googleTranslateElementInit = googleTranslateElementInit;
   }, []);
 
-    return (
-        <div id="google_translate_element"></div>
-    );
+  return <div id="google_translate_element"></div>;
 };
 
 export default GoogleTranslate;
